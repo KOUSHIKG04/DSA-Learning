e0d54b17ace17b8d0cbf636658cb559bddbdc38c
KOUSHIKGO4 <koushikgdatta5@gmail.com>
2026-06-22 18:35:21 +0530

    feat: add collection of algorithmic problem solutions to OptionalChallange.js

--- Problems/OptionalChallange.js
+++ Problems/OptionalChallange.js
@@ -3,33 +3,33 @@ function sameFrequency(num1, num2) {
   let strNum1 = num1.toString();
   let strNum2 = num2.toString();
   if (strNum1.length !== strNum2.length) return false;
- 
+
   let countNum1 = {};
   let countNum2 = {};
- 
+
   for (let i = 0; i < strNum1.length; i++) {
-    countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1
+    countNum1[strNum1[i]] = (countNum1[strNum1[i]] || 0) + 1;
   }
- 
+
   for (let j = 0; j < strNum1.length; j++) {
-    countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1
+    countNum2[strNum2[j]] = (countNum2[strNum2[j]] || 0) + 1;
   }
- 
+
   for (let key in countNum1) {
     if (countNum1[key] !== countNum2[key]) return false;
   }
- 
+
   return true;
 }
 
 // areThereDuplicates Solution (Frequency Counter)
 function areThereDuplicates() {
-  let collection = {}
+  let collection = {};
   for (let val in arguments) {
-    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1;
   }
   for (let key in collection) {
-    if (collection[key] > 1) return true
+    if (collection[key] > 1) return true;
   }
   return false;
 }
@@ -41,7 +41,7 @@ function areThereDuplicates(...args) {
     if (a > b) return 1;
     return 0;
   });
- 
+
   let start = 0;
   let next = 1;
   while (next < args.length) {
@@ -63,20 +63,20 @@ function areThereDuplicates() {
 function constructNote(message, letters) {
   var frequency = {};
   var frequencyM = {};
- 
+
   for (let i = 0; i < letters.length; i++) {
     frequency[letters[i]] = ++frequency[letters[i]] || 1;
   }
- 
+
   for (let i = 0; i < message.length; i++) {
     frequencyM[message[i]] = ++frequencyM[message[i]] || 1;
   }
- 
+
   for (let k in frequencyM) {
     if (!frequency[k]) return false;
     if (frequencyM[k] > frequency[k]) return false;
   }
- 
+
   return true;
 }
 
@@ -85,27 +85,23 @@ function findAllDuplicates(nums) {
   let ans = [];
   var s = new Set();
   for (let i = 0; i < nums.length; i++) {
-    s.has(nums[i]) ? ans.push(nums[i]) : s.add(nums[i])
+    s.has(nums[i]) ? ans.push(nums[i]) : s.add(nums[i]);
   }
   return ans;
 }
 
-
-
-
 // averagePair Solution
 function averagePair(arr, num) {
-  let start = 0
+  let start = 0;
   let end = arr.length - 1;
   while (start < end) {
-    let avg = (arr[start] + arr[end]) / 2
+    let avg = (arr[start] + arr[end]) / 2;
     if (avg === num) return true;
-    else if (avg < num) start++
-    else end--
+    else if (avg < num) start++;
+    else end--;
   }
   return false;
 }
-
 
 // isSubsequence Solution - Iterative
 function isSubsequence(str1, str2) {
@@ -120,13 +116,12 @@ function isSubsequence(str1, str2) {
   return false;
 }
 
-
 // isSubsequence Solution - Recursive but not O(1) Space
 function isSubsequence(str1, str2) {
-  if (str1.length === 0) return true
-  if (str2.length === 0) return false
-  if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))
-  return isSubsequence(str1, str2.slice(1))
+  if (str1.length === 0) return true;
+  if (str2.length === 0) return false;
+  if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1));
+  return isSubsequence(str1, str2.slice(1));
 }
 
 // findPair Solution O(n) space + O(n) time
@@ -142,20 +137,20 @@ function findPair(arr, n) {
     }
     return false;
   }
- 
+
   // for non-zero n, place all elements in a set
   const setVals = new Set(arr);
- 
+
   // check for val + n or val - n in the set
   for (let val of arr) {
     if (setVals.has(val + n) || setVals.has(val - n)) {
       return true;
     }
   }
- 
+
   return false;
 }
- 
+
 // O(1) space + O(n log n) time
 function findPair(arr, num) {
   arr.sort((a, b) => a - b);
@@ -163,34 +158,31 @@ function findPair(arr, num) {
   let j = 1;
   while (i < arr.length && j < arr.length) {
     if (i !== j && Math.abs(arr[j] - arr[i]) === Math.abs(num)) {
-      return true
+      return true;
     } else if (arr[j] - arr[i] < num) {
-      j++
+      j++;
     } else {
-      i++
+      i++;
     }
   }
   return false;
 }
 
-
-
 // maxSubArray Solution
-function maxSubarraySum(arr, num){
-    if (arr.length < num) return null;
- 
-    let total = 0;
-    for (let i=0; i<num; i++){
-       total += arr[i];
-    }
-    let currentTotal = total;
-    for (let i = num; i < arr.length; i++) {
-       currentTotal += arr[i] - arr[i-num];
-       total = Math.max(total, currentTotal);
-    }
-    return total;
-}
+function maxSubarraySum(arr, num) {
+  if (arr.length < num) return null;
 
+  let total = 0;
+  for (let i = 0; i < num; i++) {
+    total += arr[i];
+  }
+  let currentTotal = total;
+  for (let i = num; i < arr.length; i++) {
+    currentTotal += arr[i] - arr[i - num];
+    total = Math.max(total, currentTotal);
+  }
+  return total;
+}
 
 // minSubArrayLen Solution
 function minSubArrayLen(nums, sum) {
@@ -198,45 +190,43 @@ function minSubArrayLen(nums, sum) {
   let start = 0;
   let end = 0;
   let minLen = Infinity;
- 
+
   while (start < nums.length) {
-    // if current window doesn't add up to the given sum then 
-		// move the window to right
-    if(total < sum && end < nums.length){
+    // if current window doesn't add up to the given sum then
+    // move the window to right
+    if (total < sum && end < nums.length) {
       total += nums[end];
-			end++;
+      end++;
     }
     // if current window adds up to at least the sum given then
-		// we can shrink the window 
-    else if(total >= sum){
-      minLen = Math.min(minLen, end-start);
-			total -= nums[start];
-			start++;
-    } 
-    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+    // we can shrink the window
+    else if (total >= sum) {
+      minLen = Math.min(minLen, end - start);
+      total -= nums[start];
+      start++;
+    }
+    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
     else {
       break;
     }
   }
- 
+
   return minLen === Infinity ? 0 : minLen;
 }
 
-
-
-// minSubArrayLen Solution 
+// minSubArrayLen Solution
 function minSubArrayLen(nums, target) {
   let left = 0;
   let sum = 0;
   let minLen = Infinity;
 
   for (let right = 0; right < nums.length; right++) {
-    sum += nums[right]; 
+    sum += nums[right];
 
     while (sum >= target) {
-      minLen = Math.min(minLen, right - left + 1); 
-      sum -= nums[left];  
-      left++; 
+      minLen = Math.min(minLen, right - left + 1);
+      sum -= nums[left];
+      left++;
     }
   }
 
@@ -248,7 +238,7 @@ function findLongestSubstring(str) {
   let longest = 0;
   let seen = {};
   let start = 0;
- 
+
   for (let i = 0; i < str.length; i++) {
     let char = str[i];
     if (seen[char]) {
@@ -260,4 +250,198 @@ function findLongestSubstring(str) {
     seen[char] = i + 1;
   }
   return longest;
+}
+
+// Reverse Solution
+
+function reverse(str) {
+  if (str.length <= 1) return str;
+  return reverse(str.slice(1)) + str[0];
+}
+
+// isPalindrome Solution
+function isPalindrome(str) {
+  if (str.length === 1) return true;
+  if (str.length === 2) return str[0] === str[1];
+  if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1));
+  return false;
+}
+
+// someRecursive Solution
+function someRecursive(array, callback) {
+  if (array.length === 0) return false;
+  if (callback(array[0])) return true;
+  return someRecursive(array.slice(1), callback);
+}
+
+// flatten Solution
+function flatten(oldArr) {
+  var newArr = [];
+  for (var i = 0; i < oldArr.length; i++) {
+    if (Array.isArray(oldArr[i])) {
+      newArr = newArr.concat(flatten(oldArr[i]));
+    } else {
+      newArr.push(oldArr[i]);
+    }
+  }
+  return newArr;
+}
+
+// Recursion CHALLENGE Problem Set Solutions Part 2
+// capitalizeWords Solution
+function capitalizeWords(array) {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()];
+  }
+  let res = capitalizeWords(array.slice(0, -1));
+  res.push(array.slice(array.length - 1)[0].toUpperCase());
+  return res;
+}
+
+function capitalizeWords(arr) {
+  if (arr.length === 0) return [];
+
+  //   let newArr = []
+  //   for(let str of arr){
+  //       newArr.push(str.toUpperCase())
+  //   }
+  //   console.log(newArr)
+
+  return [arr[0].toUpperCase(), ...capitalizeWords(arr.slice(1))];
+}
+
+// nestedEvenSum Solution
+function nestedEvenSum(obj, sum = 0) {
+  for (var key in obj) {
+    if (typeof obj[key] === "object") {
+      sum += nestedEvenSum(obj[key]);
+    } else if (typeof obj[key] === "number" && obj[key] % 2 === 0) {
+      sum += obj[key];
+    }
+  }
+  return sum;
+}
+
+function nestedEvenSum(obj) {
+  let sum = 0;
+  let values = Object.values(obj);
+
+  for (let value of values) {
+    if (typeof value === "number" && value % 2 === 0) {
+      sum += value;
+    } else if (typeof value === "object" && value !== null) {
+      sum += nestedEvenSum(value);
+    }
+  }
+
+  return sum;
+}
+
+// capitalizeFirst Solution
+function capitalizeFirst(array) {
+  if (array.length === 1) {
+    return [array[0][0].toUpperCase() + array[0].substr(1)];
+  }
+  const res = capitalizeFirst(array.slice(0, -1));
+  const string =
+    array.slice(array.length - 1)[0][0].toUpperCase() +
+    array.slice(array.length - 1)[0].substr(1);
+  res.push(string);
+  return res;
+}
+
+function capitalizeFirst(arr) {
+  if (arr.length === 0) return [];
+
+  return [
+    arr[0].charAt(0).toUpperCase() + arr[0].slice(1),
+    ...capitalizeFirst(arr.slice(1)),
+  ];
+}
+
+// stringifyNumbers Solution
+function stringifyNumbers(obj) {
+  var newObj = {};
+  for (var key in obj) {
+    if (typeof obj[key] === "number") {
+      newObj[key] = obj[key].toString();
+    } else if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
+
+function stringifyNumbers(obj) {
+  let newObj = {};
+
+  for (let key in obj) {
+    if (typeof obj[key] === "number") {
+      newObj[key] = String(obj[key]);
+    } else if (
+      typeof obj[key] === "object" &&
+      obj[key] !== null &&
+      !Array.isArray(obj[key])
+    ) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+
+  return newObj;
+}
+
+// collectStrings Solution: Helper Method Recursion Version
+function collectStrings(obj) {
+  var stringsArr = [];
+
+  function gatherStrings(o) {
+    for (var key in o) {
+      if (typeof o[key] === "string") {
+        stringsArr.push(o[key]);
+      } else if (typeof o[key] === "object") {
+        return gatherStrings(o[key]);
+      }
+    }
+  }
+
+  gatherStrings(obj);
+
+  return stringsArr;
+}
+
+// collectStrings Solution: Pure Recursion Version
+function collectStrings(obj) {
+  var stringsArr = [];
+  for (var key in obj) {
+    if (typeof obj[key] === "string") {
+      stringsArr.push(obj[key]);
+    } else if (typeof obj[key] === "object") {
+      stringsArr = stringsArr.concat(collectStrings(obj[key]));
+    }
+  }
+
+  return stringsArr;
+}
+
+function collectStrings(obj) {
+  let resArr = [];
+  let strings = Object.values(obj);
+
+  for (let strValue of strings) {
+    if (typeof strValue === "string") {
+      resArr.push(strValue);
+    } else if (
+      typeof strValue === "object" &&
+      strValue !== null &&
+      !Array.isArray(strValue)
+    ) {
+      resArr.push(...collectStrings(strValue));
+    }
+  }
+
+  return resArr;
 }
