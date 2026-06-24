c7beb7ff8ef3684a37584894f29f9d123a19d7f0
KOUSHIKGO4 <koushikgdatta5@gmail.com>
2026-06-24 15:14:24 +0530

    feat: add algorithmic challenge solutions to OptionalChallange.js

--- Problems/OptionalChallange.js
+++ Problems/OptionalChallange.js
@@ -474,3 +474,26 @@ function name(arr, val) {
 }
 
 
+// Binary search -> for sorted array
+function binarySearch(arr, val) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === val) {
+            return mid;
+        }
+
+        if (arr[mid] < val) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+
